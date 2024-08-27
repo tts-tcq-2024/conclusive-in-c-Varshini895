6907b88a654bf37619dbeb9044c13aaad7d81bf2
Varshini895 <varshinireddynallamilli@gmail.com>
2024-08-27 10:20:39 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,32 +1,45 @@
 #pragma once
 
 typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
+  PASSIVE_COOLING,     // Passive cooling method
+  HI_ACTIVE_COOLING,   // High active cooling method
+  MED_ACTIVE_COOLING   // Medium active cooling method
 } CoolingType;
 
 typedef enum {
-  NORMAL,
-  TOO_LOW,
-  TOO_HIGH
+  NORMAL,    // Temperature is within the normal range
+  TOO_LOW,   // Temperature is below the lower limit
+  TOO_HIGH   // Temperature is above the upper limit
 } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+typedef struct {
+  int lowerLimit;    // Lower temperature limit
+  int upperLimit;    // Upper temperature limit
+} TemperatureRange;
+
+BreachType inferBreach(double value, TemperatureRange range);
+
+// Function to classify the breach type based on the cooling type and measured temperature
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
+// Function to retrieve the temperature range for a specific cooling type
+TemperatureRange getTemperatureRange(CoolingType coolingType);
+
 typedef enum {
-  TO_CONTROLLER,
-  TO_EMAIL
+  TO_CONTROLLER,  // Send alert to the controller
+  TO_EMAIL        // Send alert via email
 } AlertTarget;
 
 typedef struct {
-  CoolingType coolingType;
-  char brand[48];
+  CoolingType coolingType;  // Type of cooling used by the battery
+  char brand[48];           // Brand name of the battery (48-character array)
 } BatteryCharacter;
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+// Function to check the temperature and send an alert if there is a breach
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
+// Function to send the breach type to a controller
 void sendToController(BreachType breachType);
+
+// Function to send an email alert with the breach type
 void sendToEmail(BreachType breachType);
